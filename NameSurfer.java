ab7ba57f7530d0f68e7010064435c4342253868a
CS106A <>
2018-05-21 11:52:05 -0700

    {"types":"C","runs":17}

--- NameSurfer.java
+++ NameSurfer.java
@@ -19,7 +19,7 @@ public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
 	 */
 	public void init() {
 		// You fill this in, along with any helper methods //
-		print("hello world")l
+		print("hello world");
 	}
 
 	/**
