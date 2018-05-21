7391d070a9c6c366ffe0aca6ad3174719242a130
CS106A <>
2018-05-21 09:06:41 -0700

    {"types":"C","runs":14}

--- NameSurfer.java
+++ NameSurfer.java
@@ -11,26 +11,56 @@ import javax.swing.*;
 
 public class NameSurfer extends GraphicsProgram implements NameSurferConstants, ComponentListener {
 
-/* Method: init() */
-/**
- * This method has the responsibility for reading in the data base
- * and initializing the interactors at the top of the window.
- */
+	/* Method: init() */
+	/**
+	 * This method has the responsibility for reading in the data base
+	 * and initializing the interactors at the top of the window.
+	 */
 	public void init() {
 		addComponentListener(this);
-	    // You fill this in, along with any helper methods //
+		// You fill this in, along with any helper methods //
 	}
 
-/* Method: actionPerformed(e) */
-/**
- * This class is responsible for detecting when the buttons are
- * clicked, so you will have to define a method to respond to
- * button actions.
- */
+	/**
+	 * Clears the list of name surfer entries stored inside this class.
+	 */
+	public void clear() {
+		// You fill this in //
+	}
+
+
+	/* Method: addEntry(entry) */
+	/**
+	 * Adds a new NameSurferEntry to the list of entries on the display.
+	 * Note that this method does not actually draw the graph, but
+	 * simply stores the entry; the graph is drawn by calling update.
+	 */
+	public void addEntry(NameSurferEntry entry) {
+		// You fill this in //
+	}
+
+
+	/**
+	 * Updates the display image by deleting all the graphical objects
+	 * from the canvas and then reassembling the display according to
+	 * the list of entries. Your application must call update after
+	 * calling either clear or addEntry; update is also called whenever
+	 * the size of the canvas changes.
+	 */
+	public void update() {
+		// You fill this in //
+	}
+
+	/* Method: actionPerformed(e) */
+	/**
+	 * This class is responsible for detecting when the buttons are
+	 * clicked, so you will have to define a method to respond to
+	 * button actions.
+	 */
 	public void actionPerformed(ActionEvent e) {
 		// You fill this in //
 	}
-	
+
 	public void componentHidden(ComponentEvent e) { }
 	public void componentMoved(ComponentEvent e) { }
 	public void componentResized(ComponentEvent e) { update(); }
