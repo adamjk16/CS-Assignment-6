9e4e74bab0b2968fc5d1f29858c4c81057997b1d
CS106A <>
2019-03-01 13:17:11 -0800

    {"types":"A","runs":232}

---
+++ VoteCounter.java
@@ -0,0 +1,32 @@
+/*
+ * File: VoteCounter.java
+ * ---------------------
+ * A sandcastle program that uses collections to tally votes 
+ */
+
+import acm.program.*;
+import java.util.*;
+
+public class VoteCounter extends ConsoleProgram {
+	public void run() {
+		ArrayList<String> votes = new ArrayList<String>();
+		votes.add("Zaphod Beeblebrox");
+		votes.add("Arthur Dent");
+		votes.add("Trillian McMillian");
+		votes.add("Zaphod Beeblebrox");
+		votes.add("Marvin");
+		votes.add("Mr. Zarniwoop");
+		votes.add("Trillian McMillian");
+		votes.add("Zaphod Beeblebrox");
+		printVoteCounts(votes);
+		
+	}
+	
+	/*
+	 * Your job is to implement this method according to 
+	 * the problem specification. 
+	 */
+	private void printVoteCounts(ArrayList<String> votes) {
+		// TODO
+	}
+}
