0ded2a0383683f42c5be8aa15ce4db95f7dd7389
CS106A <>
2019-03-02 11:49:26 -0800

    {"types":"C","runs":244}

--- NameSurfer.java
+++ NameSurfer.java
@@ -43,7 +43,7 @@ public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
 		if (actionCmd.equals("Graph")) {
 			//graph???
 		} else { 
-		
+			remove.nameLabel();
 			???
 		}
 	}
