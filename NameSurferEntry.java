1dba8d2375d7a1be8da7adf229fc2534bb5176d4
CS106A <>
2019-03-10 21:28:54 -0700

    {"types":"C","runs":0}

--- NameSurferEntry.java
+++ NameSurferEntry.java
@@ -51,7 +51,7 @@ public class NameSurferEntry implements NameSurferConstants {
 	 * not appear in a decade, the rank value is 0.
 	 */
 	public int getRank(int decade) {
-		if (decade > NDECADES) {
+		if (decade >= NDECADES) {
 			return -1;
 		}
 		return rank[NDECADES];
